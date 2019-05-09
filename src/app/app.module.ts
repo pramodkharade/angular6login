55a576f35eeda880d156469743aaf5858220b86d
pramod.kharade <pramod.kharade@blazeclan.com>
2019-05-09 12:36:49 +0530

    configure route and create auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
-
+import { HomeComponent } from './home/home.component';
+import { AuthService} from './services/auth.service';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    AdminComponent
+    AdminComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([{
+      path: '',
+      component: HomeComponent
+    },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+{
+  path: 'admin',
+  component: AdminComponent
+}])
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+  getuserDetails() {
+    /**
+     * *
+     * *
+     * Post these details to get correct user info
+     * *
+     * *
+     * *
+     **/
+  }
+}
